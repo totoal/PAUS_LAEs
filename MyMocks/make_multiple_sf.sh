b15f70be7fa224e7f5a7fee6c6a6345a22727c6b
totoal <ttalbertott@gmail.com>
2023-05-07 18:35:55 +0200

    Path to script fixed

--- MyMocks/make_multiple_sf.sh
+++ MyMocks/make_multiple_sf.sh
@@ -5,7 +5,7 @@ declare -i N=1
 for i in {1..2}; do
   echo -e "\nROUND $i\n"
   for j in {1..16}; do
-    /home/alberto/cosmos/LAEs/MyMocks/Make_LAE.py $N&
+    /home/alberto/cosmos/PAUS_LAEs/MyMocks/Make_LAE.py $N&
     N=$((N + 1))
   done
   wait
