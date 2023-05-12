72705660361adc02453a245fb4dbfc9e56d7a30a
totoal <ttalbertott@gmail.com>
2023-05-12 17:26:20 +0200

    Minor changes to some functions to adapt them to PAUS

--- LAE_selection_method.py
+++ LAE_selection_method.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from paus_utils import *
-from jpasLAEs import flux_to_mag
+from jpasLAEs.utils import flux_to_mag
 
 # Line rest-frame wavelengths (Angstroms)
 w_lya = 1215.67
@@ -30,23 +30,20 @@ def estimate_continuum(NB_flx, NB_err, N_nb=7, IGM_T_correct=True,
     cont_est = np.ones(NB_flx.shape) * 99.
     cont_err = np.ones(NB_flx.shape) * 99.
 
-    for nb_idx in range(NB_flx.shape[0]):
-        # Limits on where to make the estimation
-        if nb_idx < N_nb_min:
-            continue
-        if nb_idx > N_nb_max:
-            break
-
+    for nb_idx in range(N_nb_min, N_nb_max + 1):
         if (nb_idx < N_nb) or only_right:
-            if IGM_T_correct:
+            if IGM_T_correct and nb_idx > 0:
                 IGM_T = IGM_TRANSMISSION(
                     np.array(w_central[: nb_idx - 1])
                 ).reshape(-1, 1)
             else:
                 IGM_T = 1.
-
+            
             # Stack filters at both sides or only at the right of the central one
-            if not only_right:
+            if only_right or (nb_idx == 0):
+                NBs_to_avg = NB_flx[nb_idx + 2: nb_idx + N_nb + 1]
+                NBs_errs = NB_err[nb_idx + 2: nb_idx + N_nb + 1]
+            else:
                 NBs_to_avg = np.vstack((
                     NB_flx[: nb_idx - 1] / IGM_T,
                     NB_flx[nb_idx + 2: nb_idx + N_nb + 1]
@@ -55,11 +52,8 @@ def estimate_continuum(NB_flx, NB_err, N_nb=7, IGM_T_correct=True,
                     NB_err[: nb_idx - 1] / IGM_T,
                     NB_err[nb_idx + 2: nb_idx + N_nb + 1]
                 ))
-            if only_right:
-                NBs_to_avg = NB_flx[nb_idx + 2: nb_idx + N_nb + 1]
-                NBs_errs = NB_err[nb_idx + 2: nb_idx + N_nb + 1]
 
-        if (N_nb <= nb_idx < (NB_flx.shape[0] - 6)) and not only_right:
+        if (N_nb <= nb_idx) & (nb_idx < (NB_flx.shape[0] - N_nb)) and not only_right:
             if IGM_T_correct:
                 IGM_T = IGM_TRANSMISSION(
                     np.array(w_central[nb_idx - N_nb: nb_idx - 1])
@@ -75,7 +69,7 @@ def estimate_continuum(NB_flx, NB_err, N_nb=7, IGM_T_correct=True,
                 NB_err[nb_idx + 2: nb_idx + N_nb + 1]
             ))
 
-        if nb_idx >= (NB_flx.shape[0] - 6):
+        if nb_idx >= (NB_flx.shape[0] - N_nb):
             if IGM_T_correct:
                 IGM_T = IGM_TRANSMISSION(
                     np.array(w_central[nb_idx - N_nb: nb_idx - 1])
@@ -105,7 +99,7 @@ def estimate_continuum(NB_flx, NB_err, N_nb=7, IGM_T_correct=True,
 def is_there_line(pm_flx, pm_err, cont_est, cont_err, ew0min,
                   mask=None, obs=False, sigma=3):
     if not obs:
-        z_nb_Arr = (w_central / w_lya - 1).reshape(-1, 1)
+        z_nb_Arr = (w_central[:40] / w_lya - 1)
         ew_Arr = ew0min * (1 + z_nb_Arr)
     if obs:
         ew_Arr = ew0min
@@ -116,15 +110,15 @@ def is_there_line(pm_flx, pm_err, cont_est, cont_err, ew0min,
     line = (
         # 3-sigma flux excess
         (
-            pm_flx[:-4] - cont_est > sigma * \
-            (pm_err[:-4]**2 + cont_err**2) ** 0.5
+            pm_flx[:40] - cont_est > sigma * \
+            (pm_err[:40]**2 + cont_err**2) ** 0.5
         )
         # EW0 min threshold
         & (
-            pm_flx[:-4] / cont_est > 1 + ew_Arr / fwhm_Arr
+            pm_flx[:40] / cont_est > 1 + (ew_Arr / fwhm_Arr[:40]).reshape(-1, 1)
         )
         & (
-            pm_flx[:-4] > cont_est
+            pm_flx[:40] > cont_est
         )
         # Masks
         & (
@@ -138,80 +132,6 @@ def is_there_line(pm_flx, pm_err, cont_est, cont_err, ew0min,
     return line
 
 
-# def identify_lines(line_Arr, qso_flx, cont_flx, nb_min=0, first=False):
-#     '''
-#     Returns a list of N lists with the index positions of the lines.
-
-#     Input: 
-#     line_Arr: Bool array of 3sigma detections in sources. Dim N_filters x N_sources
-#     qso_flx:  Flambda data
-#     nb_min
-#     '''
-#     N_fil, N_src = line_Arr.shape
-#     line_list = []
-#     line_len_list = []
-#     line_cont_list = []
-
-#     for src in range(N_src):
-#         fil = 0
-#         this_src_lines = []  # The list of lines
-#         this_cont_lines = []  # The list of continuum indices of lines
-#         this_src_line_flx = []  # The list of lengths of this src lines
-
-#         while fil < N_fil:
-#             this_line = []  # The list of contiguous indices of this line
-#             while ~line_Arr[fil, src]:
-#                 fil += 1
-#                 if fil == N_fil - 1:
-#                     break
-#             if fil == N_fil - 1:
-#                 break
-#             while line_Arr[fil, src]:
-#                 this_line.append(fil)
-#                 fil += 1
-#                 if fil == N_fil - 1:
-#                     break
-#             if fil == N_fil - 1:
-#                 break
-
-#             aux = -len(this_line) + nb_min + fil
-
-#             if first:  # If first=True, append continuum index to list
-#                 this_cont_lines.append(
-#                     np.average(
-#                         np.array(this_line),
-#                         weights=qso_flx[np.array(this_line), src] ** 2
-#                     )
-#                 )
-#             # Append index of the max flux of this line to the list
-#             this_src_lines.append(
-#                 np.argmax(qso_flx[np.array(this_line) + nb_min, src]) + aux
-#             )
-#             this_src_line_flx.append(
-#                 qso_flx[np.array(this_line) + nb_min, src].sum())
-
-#         if first:
-#             try:
-#                 idx = np.argmax(
-#                     np.array(this_src_line_flx)
-#                     - cont_flx[np.array(this_src_lines), src]
-#                 )
-
-#                 line_list.append(this_src_lines[idx])
-#                 line_len_list.append(this_src_lines)
-#                 line_cont_list.append(this_cont_lines[idx])
-#             except:
-#                 line_list.append(-1)
-#                 line_len_list.append([-1])
-#                 line_cont_list.append(-1)
-
-#         if not first:
-#             line_list.append(this_src_lines)
-
-#     return line_list
-
-
-# THIS IS THE "OPTIMIZED" VERSION OF CHATGPT:
 def identify_lines(line_Arr, qso_flx, cont_flx, nb_min=0, first=False):
     '''
     Returns a list of N lists with the index positions of the lines.
@@ -227,45 +147,50 @@ def identify_lines(line_Arr, qso_flx, cont_flx, nb_min=0, first=False):
     line_cont_list = []
 
     for src in range(N_src):
+        fil = 0
         this_src_lines = []  # The list of lines
         this_cont_lines = []  # The list of continuum indices of lines
         this_src_line_flx = []  # The list of lengths of this src lines
 
-        line_idx = np.where(line_Arr[:, src])[0]
-        if len(line_idx) == 0:
-            line_list.append(-1)
-            line_len_list.append([-1])
-            line_cont_list.append(-1)
-            continue
+        while fil < N_fil:
+            this_line = []  # The list of contiguous indices of this line
+            while ~line_Arr[fil, src]:
+                fil += 1
+                if fil == N_fil - 1:
+                    break
+            if fil == N_fil - 1:
+                break
+            while line_Arr[fil, src]:
+                this_line.append(fil)
+                fil += 1
+                if fil == N_fil - 1:
+                    break
+            if fil == N_fil - 1:
+                break
 
-        for i, idx in enumerate(line_idx):
-            if i == 0:
-                prev_idx = -1
-            else:
-                prev_idx = line_idx[i-1]
-            next_idx = line_idx[i+1] if i < len(line_idx)-1 else N_fil
-
-            this_line = np.arange(idx+1, next_idx)[qso_flx[idx+1:next_idx, src] > 0]
-            if len(this_line) == 0:
-                continue
-
-            aux = -len(this_line) + nb_min + idx
+            aux = -len(this_line) + nb_min + fil
 
             if first:  # If first=True, append continuum index to list
                 this_cont_lines.append(
                     np.average(
-                        this_line,
-                        weights=qso_flx[this_line, src] ** 2
+                        np.array(this_line),
+                        weights=qso_flx[np.array(this_line), src] ** 2
                     )
                 )
-
             # Append index of the max flux of this line to the list
-            this_src_lines.append(np.argmax(qso_flx[this_line, src]) + aux)
-            this_src_line_flx.append(qso_flx[this_line, src].sum())
+            this_src_lines.append(
+                np.argmax(qso_flx[np.array(this_line) + nb_min, src]) + aux
+            )
+            this_src_line_flx.append(
+                qso_flx[np.array(this_line) + nb_min, src].sum())
 
         if first:
             try:
-                idx = np.argmax(np.array(this_src_line_flx) - cont_flx[this_src_lines, src])
+                idx = np.argmax(
+                    np.array(this_src_line_flx)
+                    - cont_flx[np.array(this_src_lines), src]
+                )
+
                 line_list.append(this_src_lines[idx])
                 line_len_list.append(this_src_lines)
                 line_cont_list.append(this_cont_lines[idx])
@@ -273,11 +198,14 @@ def identify_lines(line_Arr, qso_flx, cont_flx, nb_min=0, first=False):
                 line_list.append(-1)
                 line_len_list.append([-1])
                 line_cont_list.append(-1)
-        else:
+
+        if not first:
             line_list.append(this_src_lines)
 
-    return line_list
-
+    if first:
+        return np.array(line_list)
+    else:
+        return line_list
 
 
 def nice_lya_select(lya_lines, other_lines, pm_flx, z_Arr, mask=None):
