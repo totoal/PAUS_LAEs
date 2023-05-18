db78e89efdbbc40044211495b88bb05586cc5b63
totoal <ttalbertott@gmail.com>
2023-05-18 16:08:33 +0200

    Functions prepared to load all three mocks

--- load_paus_mocks.py
+++ load_paus_mocks.py
@@ -1,20 +1,90 @@
+import pandas as pd
+import glob
+from paus_utils import w_central
+from jpasLAEs.utils import flux_to_mag, mag_to_flux
+import numpy as np
+from astropy.table import Table
+
 '''
 Functions to load the mocks.
 The mocks must be dictionaries with this structure:
     'flx_0': (46, N_sources) matrix with the fluxes
-    'mock_z': (N_sources,) Array with the true redshifts of the sources
+    'zspec': (N_sources,) Array with the true redshifts of the sources
     'L_lya': (N_sources,) Array with the intrinsic Lya Luminosities
     'EW_lya': (N_sources,) Array with the intrinsic Lya rest-frame EWs
 '''
 
 def load_qso_mock(path_to_mock):
-    return
+    files = glob.glob(f'{path_to_mock}/data*')
+    files.sort()
+    fi = []
+
+    for name in files:
+        fi.append(pd.read_csv(name))
+
+    qso_data = pd.concat(fi, axis=0, ignore_index=True)
+
+    cat = {}
+
+    cat['flx_0'] = qso_data.to_numpy()[:, 1 : 1 + 46].T
+
+    cat['zspec'] = qso_data['zspec']
+    cat['L_lya'] = qso_data['L_lya']
+    cat['EW0_lya'] = qso_data['EW0']
+
+    return cat
 
 def load_sfg_mock(path_to_mock):
-    return
+    files = glob.glob(f'{path_to_mock}/data*')
+    files.sort()
+    fi = []
 
-def load_gal_mock(path_to_mock):
-    return
+    for name in files:
+        fi.append(pd.read_csv(name))
+
+    sfg_data = pd.concat(fi, axis=0, ignore_index=True)
+
+    cat = {}
+
+
+    cat['flx_0'] = sfg_data.to_numpy()[:, 1 : 1 + 46].T
+
+    cat['zspec'] = sfg_data['zspec']
+    cat['L_lya'] = sfg_data['L_lya']
+    cat['EW0_lya'] = sfg_data['EW0']
+
+    return cat
+
+def load_gal_mock(path_to_mock, cat_fraction):
+    tab = Table.read(path_to_mock).to_pandas().to_numpy()
+    mock_size = len(tab)
+
+    np.random.seed(1312)
+    sel = np.random.randint(0, mock_size, size=int(mock_size * cat_fraction))
+    
+    cat = {}
+
+    cat['flx_0'] = mag_to_flux(tab[sel, 11 : 11 + 40],
+                                    w_central[:-6]).T
+
+    # Add BBs
+    cat['flx_0'] = np.vstack([cat['flx_0'],
+                             mag_to_flux(tab[sel, -5:].T,
+                                         w_central[-6:-1].reshape(-1,1)),
+                             np.zeros(len(sel))])
+
+    # Precompute r and mask by r. Otherwise the mock is too large
+    r_mag = flux_to_mag(cat['flx'][-4], w_central[-4])
+    mag_mask = np.array(r_mag < 24.5)
+    cat['flx_0'] = cat['flx_0'][:, mag_mask]
+
+    cat['zspec'] = np.array(tab[:, 4])[sel][mag_mask]
+    
+    # L_lya and EW0_lya are zero for all these objects.
+    cat['L_lya'] = np.zeros_like(cat['zspec'])
+    cat['EW0_lya'] = np.zeros_like(cat['zspec'])
+
+    return cat
 
 
 def load_mocks_dict(mock_SFG_path, mock_QSO_cont_path, mock_QSO_LAEs_loL_path,
