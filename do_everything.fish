6bcbf98ca2b4008e137318e98dc9a39f5f7855b0
totoal <ttalbertott@gmail.com>
2023-05-31 09:32:06 +0200

    Checking Python exceptions in a fancier way

--- do_everything.fish
+++ do_everything.fish
@@ -1,25 +1,21 @@
 #!/bin/fish
 
+function check_py_exception
+    # Check the exit code of the Python script
+    if test $status -ne 0
+        echo "Python exception occurred. Exiting..."
+        exit 1
+    end
+end
+
 # set -l nb_list "0 10"
 set -l nb_list  "0 2" "2 4" "4 6" "6 8" "8 10" "10 12" "12 14" "14 16"
 for nb in $nb_list
     /home/alberto/cosmos/PAUS_LAEs/PAUS_Lya_LF_corrections.py $nb
-    # Check the exit code of the Python script
-    if test $status -ne 0
-        echo "Python exception occurred. Exiting..."
-        break
-    end
+    check_py_exception
     /home/alberto/cosmos/PAUS_LAEs/Make_Lya_LF.py $nb
-    # Check the exit code of the Python script
-    if test $status -ne 0
-        echo "Python exception occurred. Exiting..."
-        break
-    end
+    check_py_exception
     /home/alberto/cosmos/PAUS_LAEs/LF_bootstrap_err.py $nb
-    # Check the exit code of the Python script
-    if test $status -ne 0
-        echo "Python exception occurred. Exiting..."
-        break
-    end
+    check_py_exception
 
 end
