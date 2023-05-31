bbc3acf095bd60a22111f95dc8259f124b40bd90
totoal <ttalbertott@gmail.com>
2023-05-31 14:55:46 +0200

    Improved prints

--- PAUS_Lya_LF_corrections.py
+++ PAUS_Lya_LF_corrections.py
@@ -112,7 +112,7 @@ def puricomp_corrections(mock_dict, L_bins, r_bins,
 
         N_sources = len(mock['zspec'])
         for k in range(N_iter):
-            if (k + 1) % 50:
+            if (k + 1) % 50 == 0:
                 print(f'{mock_name} correction matrices: {k + 1} / {N_iter}',
                       end=('\r' if k + 1 < N_iter else ''))
             # Generate random numbers
