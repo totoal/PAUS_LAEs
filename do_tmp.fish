c911c4a999a6393e821ef28a8649c00bb4f8e756
totoal <ttalbertott@gmail.com>
2023-07-11 12:13:27 +0200

    Does many things

---
+++ do_tmp.fish
@@ -0,0 +1,3 @@
+#!/bin/fish
+
+clear && py qso_phot_dr16.ipynb && cd MyMocks/ && py Make_QSO_mock.py && cd .. && py PAUS_Lya_LF_corrections.py "0 16" && cd ML_class/ && py NN_classifier.ipynb && cd .. && ./do_everything.fish && cd curve_fit/ && py schechter_fit.py
