4a71772b3e69da8d5dcd7389304e305af053b79c
totoal <ttalbertott@gmail.com>
2023-07-24 10:40:52 +0200

    Minor changes

--- do_everything.fish
+++ do_everything.fish
@@ -8,17 +8,17 @@ function check_py_exception
     end
 end
 
-# set -l nb_list  "0 2" "2 4" "4 6" "6 8" "8 10" "10 12" "12 14" "14 16" "16 18"
-# for nb in $nb_list
-#     /home/alberto/cosmos/PAUS_LAEs/PAUS_Lya_LF_corrections.py $nb
-#     check_py_exception
-#     /home/alberto/cosmos/PAUS_LAEs/Make_Lya_LF.py $nb
-#     check_py_exception
-#     /home/alberto/cosmos/PAUS_LAEs/LF_bootstrap_err.py $nb
-#     check_py_exception
-# end
+set -l nb_list  "0 2" "2 4" "4 6" "6 8" "8 10" "10 12" "12 14" "14 16" "16 18"
+for nb in $nb_list
+    /home/alberto/cosmos/PAUS_LAEs/PAUS_Lya_LF_corrections.py $nb
+    check_py_exception
+    /home/alberto/cosmos/PAUS_LAEs/Make_Lya_LF.py $nb
+    check_py_exception
+    /home/alberto/cosmos/PAUS_LAEs/LF_bootstrap_err.py $nb
+    check_py_exception
+end
 
-# py LF_bootstrap_err.py "combi"
+py LF_bootstrap_err.py "combi"
 
 for i in (seq 0 18)
     set nb "$i $i"
