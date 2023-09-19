a54815ab03268d8bfdfafd680c8c1dbba970c1d0
totoal <ttalbertott@gmail.com>
2023-09-19 10:54:25 +0200

    Checkpoint

--- do_tmp.fish
+++ do_tmp.fish
@@ -1,3 +1,4 @@
 #!/bin/fish
 
+py qso_phot_dr16.ipynb
 clear && cd MyMocks/ && py Make_QSO_mock.py && cd .. && py PAUS_Lya_LF_corrections.py "0 16" && cd ML_class/ && py NN_classifier.ipynb && cd .. && ./do_everything.fish && cd curve_fit/ && py schechter_fit.py
